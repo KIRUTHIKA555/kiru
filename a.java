5cb4bf9ec8cae82e0e6c4dbaa1566474b798fedd
KIRUTHIKA555 <kiruthikakt38@gmail.com>
2018-08-06 14:01:32 +0530

    Create a.java

---
+++ a.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class A
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+int n=sc.nextInt();
+for(i=0;i<0;i++)
+{
+System.out.println("HELLO");
+}
+}
+}
